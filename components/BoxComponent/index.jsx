5e976242aff375815eaf659bf81b2869d141bfd8
lnardon <lucaslnardon@gmail.com>
2021-01-02 17:45:29 -0300

    ✨ Add 3d boilerplate object to splash section

--- components/BoxComponent/index.jsx
+++ components/BoxComponent/index.jsx
@@ -1,31 +1,31 @@
 import React, { useRef, useState, useMemo } from "react";
 import { useFrame } from "react-three-fiber";
 import * as THREE from "three";
+import Font from "./font.json";
 
 const Box = (props) => {
   const mesh = useRef();
-
-  const [active, setActive] = useState(false);
-
   useFrame(() => {
-    mesh.current.rotation.y += 0.01;
+    mesh.current.rotation.z += 0.023;
   });
 
+  const font = new THREE.FontLoader().parse(Font);
+
+  const textOptions = {
+    font,
+    size: 1,
+    height: 0.2,
+    curveSegments: 21,
+  };
+
   return (
-    <mesh
-      {...props}
-      ref={mesh}
-      scale={active ? [2, 2, 2] : [1.5, 1.5, 1.5]}
-      rotation={[0, 0, Math.PI / 2]}
-      onClick={(e) => setActive(!active)}
-    >
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial
-        color={0xfafafa}
-        side={THREE.DoubleSide}
-        wireframe={true}
-        wireframeLinewidth={15}
-      ></meshStandardMaterial>
+    <mesh {...props} ref={mesh} rotation={[0, 0, 0]}>
+      <boxGeometry attach="geometry" args={props.size} />
+      <meshNormalMaterial
+      // color={0xfafafa}
+      // wireframe={true}
+      // wireframeLinewidth={3}
+      ></meshNormalMaterial>
     </mesh>
   );
 };
--- components/Scene/index.jsx
+++ components/Scene/index.jsx
@@ -1,22 +1,32 @@
-import React from "react";
+import React, { useRef } from "react";
 import { extend, useThree, useFrame } from "react-three-fiber";
 import { OrbitControls } from "../../node_modules/three/examples/jsm/controls/OrbitControls";
 import Box from "../BoxComponent";
-// import { Container } from './styles';
 
 extend({ OrbitControls });
 
 function Scene() {
+  const controls = useRef();
   const {
     camera,
     gl: { domElement },
   } = useThree();
+
+  useFrame((state) => controls.current.update());
+
   return (
     <>
-      <Box position={[0, 0, 0]} />
+      <Box position={[-3, 2, 0]} size={[1, 1, 1]} />
+      <Box position={[3, -2, 0]} size={[0.6, 0.6, 0.6]} />
+      <Box position={[3, 2, 0]} size={[0.35, 0.35, 0.35]} />
+      <Box position={[-3, -2, 0]} size={[0.85, 0.85, 0.85]} />
       <ambientLight intensity={0.75} />
-      {/* <spotLight position={[0, 100, 0]} angle={180} penumbra={1000} /> */}
-      <orbitControls args={[camera, domElement]} enableZoom={false} />
+      <spotLight position={[0, 30, 0]} angle={180} penumbra={100} />
+      <orbitControls
+        ref={controls}
+        args={[camera, domElement]}
+        enableZoom={false}
+      />
     </>
   );
 }
