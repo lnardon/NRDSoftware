bb220547c1874c503108f879f1c3f9d7915f8d63
lnardon <lucaslnardon@gmail.com>
2021-04-12 21:51:08 -0300

    🎨 Change basic website structure

--- components/ProjectsSection/index.jsx
+++ components/ProjectsSection/index.jsx
@@ -4,6 +4,7 @@ import { useTranslation } from "react-i18next";
 import styles from "./Projects.module.css";
 import taskListLogo from "../../public/taskListLogo.png";
 import shapeClassifierLogo from "../../public/shapeClassIcon.svg";
+import EyeballIcon from "../../public/eyeball.svg";
 
 function ProjectsSection() {
   const { t } = useTranslation();
@@ -32,32 +33,7 @@ function ProjectsSection() {
           </button>
         </div>
         <div className={styles.projectCard + " contactForm"}>
-          <img
-            src={shapeClassifierLogo}
-            alt="Logo"
-            className={styles.cardImg}
-          />
-          <h3 className={styles.projectName}>Shape Classifier</h3>
-          <button
-            className={styles.projectBtn}
-            onClick={() =>
-              window.open(
-                "https://lnardon.github.io/ShapeClassifier/",
-                "target: blank"
-              )
-            }
-          >
-            {t("projectBtnLabel")}
-          </button>
-        </div>
-        <div className={styles.projectCard + " contactForm"}>
-          <img
-            src={
-              "https://www.flaticon.com/svg/vstatic/svg/244/244309.svg?token=exp=1618109211~hmac=3982ea4a924ca3e3ecf0f16186228156"
-            }
-            alt="Logo"
-            className={styles.cardImg}
-          />
+          <img src={EyeballIcon} alt="Logo" className={styles.cardImg} />
           <h3 className={styles.projectName}>3D Eyeball</h3>
           <button
             className={styles.projectBtn}
@@ -83,6 +59,25 @@ function ProjectsSection() {
             onClick={() =>
               window.open(
                 "https://personalwebsite-5d16b.web.app/",
+                "target: blank"
+              )
+            }
+          >
+            {t("projectBtnLabel")}
+          </button>
+        </div>
+        <div className={styles.projectCard + " contactForm"}>
+          <img
+            src={shapeClassifierLogo}
+            alt="Logo"
+            className={styles.cardImg}
+          />
+          <h3 className={styles.projectName}>Shape Classifier</h3>
+          <button
+            className={styles.projectBtn}
+            onClick={() =>
+              window.open(
+                "https://lnardon.github.io/ShapeClassifier/",
                 "target: blank"
               )
             }
